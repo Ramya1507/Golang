2803bb929f143709c97b08b43765f5f53bce97fb
Ramya Sree Gorantla <Gorantla.Sree@tatacommunications.com>
2021-09-23 14:07:07 +0530

    my first project

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		return 
+	}
+	
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, " <h1>welcome to go.... <h2>")
+	if err != nil {
+		return 
+	}
+}
