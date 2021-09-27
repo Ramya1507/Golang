9780b589d7bcf2606b605ec5b14b9597cd4c3932
Ramya Sree Gorantla <Gorantla.Sree@tatacommunications.com>
2021-09-27 11:49:33 +0530

    main file

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8087", nil)
+	if err != nil {
+		return 
+	}
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello World!")
+	if err != nil {
+		return 
+	}
+}
